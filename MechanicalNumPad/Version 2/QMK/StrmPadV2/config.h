09f127c56c5969a915d71b62709ad81e98fcfc57
Inn0360 <howin.tam@gmail.com>
2022-03-27 22:20:19 +1100

    Revert Changes to Prev

--- MechanicalNumPad/Version 2/QMK/StrmPadV2/config.h
+++ MechanicalNumPad/Version 2/QMK/StrmPadV2/config.h
@@ -43,7 +43,7 @@
 #define RGBLIGHT_VAL_STEP 8
 #define RGBLIGHT_LIMIT_VAL 255 /* The maximum brightness level */
 #define RGBLIGHT_SLEEP
-#define RGBLIGHT_TIMEOUT 3600000  // 1 Hour     /* If defined, the RGB lighting will be switched off when the host goes to sleep */
+#define RGBLIGHT_TIMEOUT 60000  // 1 Hour     /* If defined, the RGB lighting will be switched off when the host goes to sleep */
 #define RGB_DISABLE_WHEN_USB_SUSPENDED true
 
 #define RGBLIGHT_ANIMATIONS
